9f8983861035a5d7d3c9f6d29089fd81c45a1906
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2025-01-06 05:54:19 +0000

    adding prompt template a document template.

--- app/prompt.py
+++ app/prompt.py
@@ -4,3 +4,13 @@
 # RetrievalQAWithSources chain and how it is initialized.
 ##############################################################################
 from langchain.prompts import PromptTemplate
+
+PROMPT = PromptTemplate(
+    template="Question: {question}\nAnswer: {answer}",
+    input_variables=["question", "answer"]
+)
+
+EXAMPLE_PROMPT = PromptTemplate(
+    template="Content: {page_content}\nSource: {source}",
+    input_variables=["page_content", "source"],
+)
